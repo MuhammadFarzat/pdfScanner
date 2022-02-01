e884b50eb79f2a3c851e19644c05afee1ce9daf1
Muhammad Farzat <Farzatmuhammad@dv-schulen.de>
2022-02-01 19:28:43 +0100

    Read to the underscole

--- index.js
+++ index.js
@@ -54,51 +54,42 @@ function getWantedFile(artikelNummer)
         success: function(response)
         {
             document.getElementById('loader').style.display = "none";
-            document.getElementById('myFrame').src=response;
-            document.getElementById('myFrame').style.display = "flex";
+           
         }
     });
 }
 //Gives us the requested file
 function dateiSuchen() {
 
-    //If the name from the user reaches 10 chars
-    //if(document.getElementById('scannID').value.length == 10)
-    //{
+        //If the name from the user reaches 10 chars
+    
         var artikelNummer = document.getElementById('scannID').value;
 
         var data = {};
+        const fileUrl = "http://localhost/pdfScanner/Umpackanweisungen/";
 
         data["artikelNummer"] = artikelNummer;
         //Is for the loading logo
-        document.getElementById('loader').style.display = "flex";
+
 
         $.ajax({
             //Sends request to dateisuchen.php to search and return text of the wanted php file
-            url: "http://localhost/pdfScanner-main/php/dateisuchen.php",
+            url: "http://localhost/pdfScanner/php/dateisuchen.php",
             type: "POST",
             data: data,
             success: function(response)
             {
-                document.getElementById('loader').style.display = "none";
+             
                 //We are interested for now only in the else statement, which gives us the text
-                if(response == "Es gibt mehrere")
-                {
-                    alert(response);
-                    getAllFiles(artikelNummer);
-                }
-                else if(response == "Keine Datei gefunden")
+                if(response == "nichts")
                 {
                     console.log(response);
                 }
                 else
                 {
-                    getWantedFile(artikelNummer);
-                    console.log(response);
+                    document.getElementById('myFrame').src = fileUrl + response;
+                    document.getElementById('myFrame').style.display = "flex";
                 }
-            }});
-
-
-     
-    //}
+            }
+        });
   }
